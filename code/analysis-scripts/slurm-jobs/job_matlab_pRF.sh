cee08323df57636a36e975c17c3005b4bba3acda
mayajas <85355294+mayajas@users.noreply.github.com>
2022-12-19 11:04:24 +0100

    make matlab slurm job a batch script

--- code/analysis-scripts/slurm-jobs/job_matlab_pRF.sh
+++ code/analysis-scripts/slurm-jobs/job_matlab_pRF.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
 
 #SBATCH --mail-user=mayaaj90@zedat.fu-berlin.de
-#SBATCH --job-name=p00d02
-#SBATCH --mail-type=end
-#SBATCH --mem=20GB
-#SBATCH --cpus-per-task=30
-#SBATCH --time=2-00:00:00
+#SBATCH --job-name=p0prf
+#SBATCH --mem=10G
+#SBATCH --cpus-per-task=20
+#SBATCH --time=1-00:00:00
 #SBATCH --qos=standard
+#SBATCH --mail-type=BEGIN,END,FAIL,ARRAY_TASKS
 
-sub=1
-depth=2
+declare -a combinations
+index=0
+for sub in 1 2 3 4
+do
+    for depth in 1 2 3 4 5 6 7 8 9
+    do
+      	combinations[$index]="$sub $depth"
+        index=$((index + 1))
+    done
+done
 
-#Extract parameters
+parameters=(${combinations[${SLURM_ARRAY_TASK_ID}]})
+
+sub=${parameters[0]}
+depth=${parameters[1]}
+
+#Print out parameters
 echo sub $sub
 echo depth $depth
 echo using $SLURM_CPUS_PER_TASK cpus
@@ -22,11 +35,9 @@ module add MATLAB/2021a
 # wait a bit so it doesn't crash
 sleep 50
 
-cd /home/mayaaj90/scripts/project-00-7t-pipeline-dev/
+# change directory to the one containing the scripts
+cd /home/mayaaj90/projects/project-00-7t-pipeline-dev/code/analysis-scripts/matlab
 
 ### Start job
-matlab -nosplash -noFigureWindows -r "pRFmapping_curta(${sub},${depth},$SLURM_CPUS_PER_TASK)" > serial.out #this worked
 echo set to run
-### Output core and memory efficiency
-
-seff $SLURM_JOBID
+matlab -nosplash -noFigureWindows -r "pRFmapping_curta($sub,$depth,$SLURM_CPUS_PER_TASK)" > serial_sub${sub}_depth${depth}.out #this worked
