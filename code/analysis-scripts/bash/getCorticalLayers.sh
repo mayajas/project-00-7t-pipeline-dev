145d4d21555f2326b4aee053170b7402b7254350
mayajas <85355294+mayajas@users.noreply.github.com>
2023-01-27 21:16:21 +0100

    add option to run laynii in func space

--- code/analysis-scripts/bash/getCorticalLayers.sh
+++ code/analysis-scripts/bash/getCorticalLayers.sh
@@ -1,14 +1,30 @@
 SUBJECT_ID="sub-0$1"
 
+# do layerification in anat space or func space?
+SPACE=func
+#anat
+#func
+
 # define directories
 LAYNII_DIR=/home/mayajas/Documents/programs/laynii
 RIM_SCRIPT_DIR=/home/mayajas/Documents/project-00-7t-pipeline-dev/code/analysis-scripts/python
-PRFPY_DIR=/home/mayajas/scratch/project-00-7t-pipeline-dev/output/prfpy/$SUBJECT_ID
-LAYER_DIR=$PRFPY_DIR/layerification
+PRFPY_DIR=/home/mayajas/scratch/project-00-7t-pipeline-dev/output/prfpy_Iso2DGaussianModel/$SUBJECT_ID
 COREG_DIR=/home/mayajas/scratch/project-00-7t-pipeline-dev/manualcorr/coreg/_subject_id_$SUBJECT_ID
+if [ "$SUBJECT_ID" = "sub-04" ]; then
+    FS_DIR=/home/mayajas/scratch/project-00-7t-pipeline-dev/derivatives/wf_advanced_skullstrip_sub-04/_subject_id_$SUBJECT_ID/autorecon_pial_rerun
+else
+    FS_DIR=/home/mayajas/scratch/project-00-7t-pipeline-dev/derivatives/wf_advanced_skullstrip/_subject_id_$SUBJECT_ID/autorecon_pial_rerun
+fi
+
+if [ "$SPACE" = "func" ]; then
+    LAYER_DIR=$PRFPY_DIR/layerification_func
+else
+    LAYER_DIR=$PRFPY_DIR/layerification
+fi
+
 
 # define anatomical and func images
-FUNC_IMG=/home/mayajas/scratch/project-00-7t-pipeline-dev/output/prfpy/$SUBJECT_ID/meanFunc.nii
+FUNC_IMG=${PRFPY_DIR}/meanFunc.nii
 
 # make layer output directory
 if [ ! -d "$LAYER_DIR" ]; then
@@ -19,36 +35,66 @@ else
 fi
 
 # convert ribbon mgz to nii
-mri_convert $PRFPY_DIR/ribbon.mgz $LAYER_DIR/ribbon.nii
+mri_convert $FS_DIR/$SUBJECT_ID/mri/ribbon.mgz $LAYER_DIR/ribbon.nii
 
-# make laynii required rim file
-python $RIM_SCRIPT_DIR/makeLayniiRim.py $LAYER_DIR/ribbon.nii
 
-# laynii layerification
-$LAYNII_DIR/LN2_LAYERS -rim $LAYER_DIR/ribbon_rim.nii -nr_layers 10 -equivol
+if [ "$SPACE" = "anat" ]; then
+    # make laynii required rim file in anat space
+    python $RIM_SCRIPT_DIR/makeLayniiRim.py $LAYER_DIR/ribbon.nii
 
-# coreg to func space
-# equidistant image:
-INPUT=$LAYER_DIR/ribbon_rim_layers_equidist.nii
-OUTPUT=$LAYER_DIR/func_ribbon_rim_layers_equidist.nii
-${ANTSPATH}antsApplyTransforms \
-  -d 3 \
-  -i $INPUT \
-  -r $FUNC_IMG \
-  -t [ $COREG_DIR/registered_0GenericAffine.mat, 1 ] \
-  -o $OUTPUT \
-  --interpolation NearestNeighbor
+    # laynii layerification
+    $LAYNII_DIR/LN2_LAYERS -rim $LAYER_DIR/ribbon_rim.nii -nr_layers 10 -equivol
 
-# equivol image:
-INPUT=$LAYER_DIR/ribbon_rim_layers_equivol.nii
-OUTPUT=$LAYER_DIR/func_ribbon_rim_layers_equivol.nii
-${ANTSPATH}antsApplyTransforms \
-  -d 3 \
-  -i $INPUT \
-  -r $FUNC_IMG \
-  -t [ $COREG_DIR/registered_0GenericAffine.mat, 1 ] \
-  -o $OUTPUT \
-  --interpolation NearestNeighbor
+    # coreg to func space
+    # equidistant image:
+    INPUT=$LAYER_DIR/ribbon_rim_layers_equidist.nii
+    OUTPUT=$LAYER_DIR/func_ribbon_rim_layers_equidist.nii
+    ${ANTSPATH}antsApplyTransforms \
+      -d 3 \
+      -i $INPUT \
+      -r $FUNC_IMG \
+      -t [ $COREG_DIR/registered_0GenericAffine.mat, 1 ] \
+      -o $OUTPUT \
+      --interpolation NearestNeighbor
+
+    # equivol image:
+    INPUT=$LAYER_DIR/ribbon_rim_layers_equivol.nii
+    OUTPUT=$LAYER_DIR/func_ribbon_rim_layers_equivol.nii
+    ${ANTSPATH}antsApplyTransforms \
+      -d 3 \
+      -i $INPUT \
+      -r $FUNC_IMG \
+      -t [ $COREG_DIR/registered_0GenericAffine.mat, 1 ] \
+      -o $OUTPUT \
+      --interpolation NearestNeighbor
+
+else
+    # coreg ribbon file to func space
+    INPUT=$LAYER_DIR/ribbon.nii
+    OUTPUT=$LAYER_DIR/func_ribbon.nii
+    ${ANTSPATH}antsApplyTransforms \
+      -d 3 \
+      -i $INPUT \
+      -r $FUNC_IMG \
+      -t [ $COREG_DIR/registered_0GenericAffine.mat, 1 ] \
+      -o $OUTPUT \
+      --interpolation NearestNeighbor
+
+    # make laynii required rim file in anat space
+    python $RIM_SCRIPT_DIR/makeLayniiRim.py $LAYER_DIR/func_ribbon.nii
+
+    # laynii layerification
+    $LAYNII_DIR/LN2_LAYERS -rim $LAYER_DIR/func_ribbon_rim.nii -nr_layers 10 -equivol
+    
+
+fi
+
+
+
+
+
+
+
 
 
 
